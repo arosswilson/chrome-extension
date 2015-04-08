20bfc078e1a6ddfa4824b2060fb52e2ea20eeac7
Lowell Mower <lowell.m.mower@gmail.com>
2015-04-08 13:21:57 -0400

    Initial commit

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // No tabs or host permissions needed!
+  console.log('Turning ' + tab.url + ' red!');
+  chrome.tabs.executeScript({
+    code: 'document.getElementById("course_13").style.backgroundColor="red"'
+  });
+});
